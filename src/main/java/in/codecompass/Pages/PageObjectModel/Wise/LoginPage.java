b22f22124aa0d84f9fdeea72eb59be4304aad130
uveshrajput-TESTER <uveshrajpute@gmail.com>
2025-12-29 23:52:59 +0530

    LoginPage complete

--- src/main/java/in/codecompass/Pages/PageObjectModel/Wise/LoginPage.java
+++ src/main/java/in/codecompass/Pages/PageObjectModel/Wise/LoginPage.java
@@ -14,16 +14,29 @@ public class LoginPage extends CommonToAllPage {
     public LoginPage(WebDriver driver){
         this.driver = driver;
     }
+
+
     // // Step 1 - Page Locators
     private final By username = By.id("normal_login_userName");
     private final By password = By.id("normal_login_password");
     private final By LoginButton = By.xpath("//div[@class = \"ant-form-item-control-input-content\"]/button");
     private final By remeberme = By.id("normal_login_remember");
    private final By error_message_invalid_cred = By.xpath("//span[normalize-space()='Invalid user credentials!... Access Denied']");
-private final By error_message_empty_username_field = By.xpath("//div[@class = \"ant-form-item-explain-error\" ]");
+   private final By error_message_empty_username_field = By.xpath("//div[@class = \"ant-form-item-explain-error\" ]");
+ private final By error_message_empty_password_field = By.xpath("//div[@id = \"normal_login_password_help\"]/div[@class=\"ant-form-item-explain-error\"]");
 
+ private final By forgot_password = By.xpath("//button[@class='ant-btn css-1x5n6pw ant-btn-link ant-btn-color-link ant-btn-variant-link ant-btn-sm login-form-forgot']");
+private final By username_forgot = By.id("username");
+private final By Email_forgot = By.id("email");
+private final By Submit_forgot = By.xpath("//button[@class='ant-btn css-1x5n6pw ant-btn-primary ant-btn-color-primary ant-btn-variant-solid ant-btn-sm ant-btn-block']");
+private final By message_successfull_forgot = By.xpath("//span[contains(text(),'Reset password link has been sent to registered emailId for user: ')]");
+private final By message_InvalidUsername_forgot = By.xpath("//span[contains(text(),'does not exist')]");
+private final By message_InvalidEmail_forgot = By.xpath("//span[contains(text(),'please enter registered email-Id')]");
+private final By message_forgot_empty_username = By.xpath("//div[contains(text(),'Please enter your username')]");
+private final By message_forgot_empty_Email = By.xpath("//div[contains(text(),'Please enter your email')]");
+private final By message_forgot_unformated_Email = By.xpath("//div[contains(text(),'Please enter a valid email')]");
 
-   //    Step 2 - Actions
+    //    Step 2 - Actions
     public String loginToWiseLoginInvalidCreds(String user, String pwd) {
         openWiseUrl();
         enterInput(username,user);
@@ -42,5 +55,85 @@ private final By error_message_empty_username_field = By.xpath("//div[@class = \
         clickElement(remeberme);
         clickElement(LoginButton);
     }
+    public String emptyUsernamefield_error(String pwd){
+        openWiseUrl();
+        enterInput(password,pwd);
+        clickElement(remeberme);
+        clickElement(LoginButton);
+        WaitHelpers.checkVisibility(driver,error_message_empty_username_field,3);
+        String message = getText(error_message_empty_username_field);
+        return message;
+   }
+    public String emptyPasswordfield_error(String user){
+        openWiseUrl();
+        enterInput(username,user);
+        clickElement(remeberme);
+        clickElement(LoginButton);
+        WaitHelpers.checkVisibility(driver,error_message_empty_username_field,3);
+        String message = getText(error_message_empty_password_field);
+        return message;
+    }
+   public String forgotpasswordvalidCreds(String user,String Email){
+        openWiseUrl();
+        clickElement(forgot_password);
+        enterInput(username_forgot,user);
+        enterInput(Email_forgot,Email);
+        clickElement(Submit_forgot);
+        WaitHelpers.checkVisibility(driver,message_successfull_forgot,30);
+        String message = getText(message_successfull_forgot);
+        return message;
+  }
+    public String forgotpasswordInvalidCreds(String user,String Email){
+        openWiseUrl();
+        clickElement(forgot_password);
+        enterInput(username_forgot,user);
+        enterInput(Email_forgot,Email);
+        clickElement(Submit_forgot);
+        WaitHelpers.checkVisibility(driver,message_InvalidUsername_forgot,20);
+        String message = getText(message_InvalidUsername_forgot);
+        return message;
+    }
+    public String forgotpasswordInvalidCreds2(String user,String Email){
+        openWiseUrl();
+        clickElement(forgot_password);
+        enterInput(username_forgot,user);
+        enterInput(Email_forgot,Email);
+        clickElement(Submit_forgot);
+        WaitHelpers.checkVisibility(driver,message_InvalidEmail_forgot,20);
+        String message = getText(message_InvalidEmail_forgot);
+        return message;
+    }
+
+    public String forgotpasswordEmptyusername(String Email){
+        openWiseUrl();
+        clickElement(forgot_password);
+        enterInput(Email_forgot,Email);
+        clickElement(Submit_forgot);
+        WaitHelpers.checkVisibility(driver,message_forgot_empty_username,20);
+        String message = getText(message_forgot_empty_username);
+        return message;
+    }
+    public String forgotpasswordEmptyEmail(String user){
+        openWiseUrl();
+        clickElement(forgot_password);
+        enterInput(username_forgot,user);
+        clickElement(Submit_forgot);
+        WaitHelpers.checkVisibility(driver,message_forgot_empty_Email,20);
+        String message = getText(message_forgot_empty_Email);
+        return message;
+    }
+    public String forgotpasswordunformatedEmail(String Email){
+        openWiseUrl();
+        clickElement(forgot_password);
+        enterInput(Email_forgot,Email);
+        clickElement(Submit_forgot);
+        WaitHelpers.checkVisibility(driver,message_forgot_unformated_Email,20);
+        String message = getText(message_forgot_unformated_Email);
+        return message;
+    }
+
+
+
+
 
 }
---
+++ src/test/java/in/codecompass/Tests/PageObjectModel_Test/WISE/CRUD/Test_Wise_Login_Page.java
@@ -0,0 +1,183 @@
+package in.codecompass.Tests.PageObjectModel_Test.WISE.CRUD;
+
+import in.codecompass.Base.CommonToAllTest;
+import in.codecompass.Driver.DriverManager;
+import in.codecompass.Pages.PageObjectModel.Wise.HomePage;
+import in.codecompass.Pages.PageObjectModel.Wise.LoginPage;
+import in.codecompass.Utils.PropertiesReader;
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.annotations.Test;
+import org.testng.Assert;
+import static org.assertj.core.api.Assertions.*;
+
+import static org.hamcrest.Matchers.containsString;
+public class Test_Wise_Login_Page extends CommonToAllTest {
+    private static final Logger logger = LogManager.getLogger(Test_Wise_Login_Page.class);
+
+    @Owner("Uvesh rajput")
+    @Description("TC#1- Verify that with valid Username and Password User login Successfully ")
+    @Test
+    public void test_Login_Successfully() throws Exception {
+        logger.info("Starting the TC#1");
+        // Page Class Code (POM Code) - 2
+        LoginPage loginPage  = new LoginPage(DriverManager.getDriver());
+        loginPage.loginToWiseLoginValidCreds(PropertiesReader.readKey("username"), PropertiesReader.readKey("password"));
+        HomePage homePage = new HomePage(DriverManager.getDriver());
+        String txt =  homePage.hometext();
+        assertThat(txt).isNotBlank().isNotNull().isNotEmpty();
+        logger.info("Done the Test cases");
+        Assert.assertEquals(txt,PropertiesReader.readKey("hometext"));
+    }
+    @Owner("Uvesh rajput")
+    @Description("TC#2- Verify that with Invalid Username and Password,Proper Message is Shown ")
+    @Test
+    public void test_Login_Error_Message() throws Exception {
+        logger.info("Starting the TC#2");
+        // Page Class Code (POM Code) - 2
+        LoginPage loginPage  = new LoginPage(DriverManager.getDriver());
+       String txt =  loginPage. loginToWiseLoginInvalidCreds(PropertiesReader.readKey("invalid_username"), PropertiesReader.readKey("invalid_password"));
+
+        assertThat(txt).isNotBlank().isNotNull().isNotEmpty();
+        logger.info("Done the Test cases");
+        Assert.assertEquals(txt,PropertiesReader.readKey("error_message"));
+    }
+
+    @Owner("Uvesh rajput")
+    @Description("TC#3- Verify that with empty Username and valid Password,Proper error Message is Shown to User ")
+    @Test
+    public void test_Useremptyfield() throws Exception {
+        logger.info("Starting the TC#3");
+        // Page Class Code (POM Code) - 2
+        LoginPage loginPage  = new LoginPage(DriverManager.getDriver());
+        String txt =  loginPage.emptyUsernamefield_error(PropertiesReader.readKey("password"));
+
+       logger.info("Assertion is Started");
+        assertThat(txt).isNotBlank().isNotNull().isNotEmpty();
+        Assert.assertEquals(txt,PropertiesReader.readKey("error_message_empty_user_field"));
+        logger.info("Done the Test cases");
+    }
+    @Owner("Uvesh rajput")
+    @Description("TC#4- Verify that with valid Username and empty Password,Proper error Message is Shown to User ")
+    @Test
+    public void test_Passwordemptyfield() throws Exception {
+        logger.info("Starting the TC#4");
+        // Page Class Code (POM Code) - 2
+        LoginPage loginPage  = new LoginPage(DriverManager.getDriver());
+        String txt =  loginPage.emptyPasswordfield_error(PropertiesReader.readKey("username"));
+
+        logger.info("Assertion is Started");
+        assertThat(txt).isNotBlank().isNotNull().isNotEmpty();
+        Assert.assertEquals(txt,PropertiesReader.readKey("error_message_empty_paswd_field"));
+        logger.info("Done the Test cases");
+    }
+    @Owner("Uvesh rajput")
+    @Description("TC#5- Verify that with valid Username and valid email User get forgot Password Email ")
+    @Test
+    public void test_forgotvalidCreds() throws Exception {
+        logger.info("Starting the TC#5");
+        // Page Class Code (POM Code) - 2
+        LoginPage loginPage  = new LoginPage(DriverManager.getDriver());
+         String txt =  loginPage.forgotpasswordvalidCreds(PropertiesReader.readKey("username_forgot"),PropertiesReader.readKey("Email_forgot"));
+
+        logger.info("Assertion is Started");
+        assertThat(txt).isNotBlank().isNotNull().isNotEmpty();
+        assertThat(txt).contains("Reset password link has been sent to registered emailId for user:");
+        logger.info("Done the Test cases");
+    }
+
+    @Owner("Uvesh rajput")
+    @Description("TC#6- Verify that with invalid Username and valid email ,User got Proper Message  ")
+    @Test
+    public void test_forgotInvaildUser() throws Exception {
+        logger.info("Starting the TC#6");
+        logger.info("TC#6- Verify that with invalid Username and valid email ,User got Proper Message ");
+        // Page Class Code (POM Code) - 2
+        LoginPage loginPage  = new LoginPage(DriverManager.getDriver());
+        String txt =  loginPage.forgotpasswordInvalidCreds(PropertiesReader.readKey("Invalid_username_forgot"),PropertiesReader.readKey("Email_forgot"));
+
+        logger.info("Assertion is Started");
+        assertThat(txt).isNotBlank().isNotNull().isNotEmpty();
+        assertThat(txt).containsIgnoringCase("does not exist");
+        logger.info("Done the Test cases");
+
+    }
+
+    @Owner("Uvesh rajput")
+    @Description("TC#7- Verify that with valid Username and Invalid email ,User got Proper Message  ")
+    @Test
+    public void test_forgotInvaildEmail() throws Exception {
+        logger.info("Starting the TC#7");
+        logger.info("TC#7- Verify that with valid Username and Invalid email ,User got Proper Message ");
+        // Page Class Code (POM Code) - 2
+        LoginPage loginPage  = new LoginPage(DriverManager.getDriver());
+        String txt =  loginPage.forgotpasswordInvalidCreds2(PropertiesReader.readKey("username_forgot"),PropertiesReader.readKey("Invalid_Email_forgot"));
+
+        logger.info("Assertion is Started");
+        assertThat(txt).isNotBlank().isNotNull().isNotEmpty();
+        assertThat(txt).containsIgnoringCase("please enter registered email-Id");
+        logger.info("Done the Test cases");
+
+    }
+    @Owner("Uvesh rajput")
+    @Description("TC#8- Verify that with empty Username and valid email ,User got Proper Message   ")
+    @Test
+    public void test_forgotEmptyUsername() throws Exception {
+        logger.info("Starting the TC#8");
+        logger.info("TC#8- Verify that with empty Username and valid email ,User got Proper Message ");
+        // Page Class Code (POM Code) - 2
+        LoginPage loginPage  = new LoginPage(DriverManager.getDriver());
+        String txt =  loginPage.forgotpasswordEmptyusername(PropertiesReader.readKey("Email_forgot"));
+
+        logger.info("Assertion is Started");
+        assertThat(txt).isNotBlank().isNotNull().isNotEmpty();
+        assertThat(txt).containsIgnoringCase("Please enter your username");
+        logger.info("Done the Test cases");
+
+    }
+    @Owner("Uvesh rajput")
+    @Description("TC#9- Verify that with valid Username and Empty email ,User got Proper Message   ")
+    @Test
+    public void test_forgotEmptyEmail() throws Exception {
+        logger.info("Starting the TC#9");
+        logger.info("TC#9- Verify that with valid Username and Empty email ,User got Proper Message ");
+        // Page Class Code (POM Code) - 2
+        LoginPage loginPage  = new LoginPage(DriverManager.getDriver());
+        String txt =  loginPage.forgotpasswordEmptyEmail(PropertiesReader.readKey("username_forgot"));
+
+        logger.info("Assertion is Started");
+        assertThat(txt).isNotBlank().isNotNull().isNotEmpty();
+        assertThat(txt).containsIgnoringCase("Please enter your email");
+        logger.info("Done the Test cases");
+
+    }
+    @Owner("Uvesh rajput")
+    @Description("TC#10- Verify that with unformated email in UI User Got Proper Message   ")
+    @Test
+    public void test_forgotUnformatedEmail() throws Exception {
+        logger.info("Starting the TC#10");
+        logger.info("TC#10- Verify that with unformated email in UI User Got Proper Message  ");
+        // Page Class Code (POM Code) - 2
+        LoginPage loginPage  = new LoginPage(DriverManager.getDriver());
+        String txt =  loginPage.forgotpasswordunformatedEmail(PropertiesReader.readKey("unformated_Email_forgot"));
+
+        logger.info("Assertion is Started");
+        assertThat(txt).isNotBlank().isNotNull().isNotEmpty();
+        assertThat(txt).containsIgnoringCase("Please enter a valid email");
+        logger.info("Done the Test cases");
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
